f254c78478ca250d81113662f23bc9aa9292670a
haticekorogllu <76959638+haticekorogllu@users.noreply.github.com>
2021-01-10 23:43:23 +0300

    Add files via upload

---
+++ Homework7/Homework7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework7
+{
+    class Program
+    {     // hocam index no bulamadım bir türlü :/ 
+        static void Main(string[] args)
+        {
+            string [] yemekler = { " içlikofte", "cigkofte", "kebap", "dolma", "lahmacun", "pide", "döner" };
+            Console.WriteLine(" Canın ne çekiyor ?");
+            string girilenYemek = Console.ReadLine();
+            bool varMi = false;
+            
+            
+             foreach (string yemek in yemekler )
+            {
+                Console.WriteLine(yemek + "-");
+                   
+                if( girilenYemek = yemekler)
+                {
+                    varMi = true;
+                    break;
+
+                }
+            }
+            Console.WriteLine();
+            if (varMi)
+            {
+                Console.WriteLine($"{girilenYemek} arrayde vardır.");
+                
+            }
+            else
+            {
+                Console.WriteLine($"{girilenYemek} arrayde yoktur.");
+
+            }
+            
+        }
+    }
+}
