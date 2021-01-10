492ba650680421a7bd1bc6f1c3b899b4c02b13f8
haticekorogllu <76959638+haticekorogllu@users.noreply.github.com>
2021-01-10 23:22:40 +0300

    Add files via upload

---
+++ Homework6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nerdeBuAsalSayilar = 0;
+            for (int sayi = 2; sayi <= 10000; sayi++)
+            {
+                int kontrol = 0;
+
+                for (int i = 2; i < sayi; i++)
+                {
+                    if (sayi % i == 0) 
+                    {
+                        kontrol = 1;
+                        break;
+                    }
+                }
+                if (kontrol == 1)
+                {
+                    Console.WriteLine("{0} asal değidir", sayi);
+                }
+                else
+                {
+                    Console.WriteLine("{0} asaldır.", sayi);
+                    nerdeBuAsalSayilar++;
+                }
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
